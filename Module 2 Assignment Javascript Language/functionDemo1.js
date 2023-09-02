d4761b2029ab31e37cfdc407df0a8a0b39857b69
skale2022 <143906602+skale2022@users.noreply.github.com>
2023-09-02 13:49:40 -0700

    Added Module 2 Assignment Javascript Language

---
+++ Module 2 Assignment Javascript Language/functionDemo1.js
@@ -0,0 +1,28 @@
+// Student Name: Saurabh Kale
+// Student ID: 1223450319
+// Date: 09/02/2023
+
+var age = 10;   // Int
+var salary = 15.00; //Double
+
+// console.log(`The variable type of age is ${typeof(age)}`);
+// console.log(`The variable type of salary is ${typeof(salary)}`);
+
+// var name = 'Joh Smith';
+// console.log(`The variable type of name is ${typeof(name)}`);
+
+const displayGreetings = function(name, year){ //parameter  // Name Function
+    console.log(`Happy new year ${year} ${name}`)
+}
+
+// console.log(`The variable type of displayGreetings is ${typeof(displayGreetings)}`);
+
+const displayGreetingsWithEMoji = function(name, year){ //parameter  // Name Function
+    console.log(`😊 😊 Happy new year ${year} ${name} 😊 😊`)
+}
+
+const greet = function(name, year, func){
+    func(name, year);
+}
+
+greet('Sam', 2022, displayGreetingsWithEMoji);
---
+++ Module 2 Assignment Javascript Language/index.js
@@ -0,0 +1,34 @@
+// Student Name: Saurabh Kale
+// Student ID: 1223450319
+// Date: 09/02/2023
+
+// function greetTheStudent(studentName){ //parameter  // Name Function
+//     return `Hello there ${studentName}`;
+// }
+
+//Fat Arrow Function
+
+// const greetTheStudent = (studentName) => { //parameter // Anonymous Function   // Expression
+//     return `Hello there ${studentName}`;
+// }
+
+const greetTheStudent = studentName => `Hello there ${studentName}`;
+
+const fullName = (firstName, middleName, lastName)=>`${lastName}, ${middleName}, ${firstName}`
+
+// const student = 'Mary Jane';
+// const greet = greetTheStudent(student); //argument
+// console.log(greet);
+
+const titleName = fullName('Mary','R','Jane')
+console.log(titleName)
+// console.log(message + ' ' + studentName);
+// console.log(message + ' ' + studentName2);
+
+
+
+
+
+
+
+
